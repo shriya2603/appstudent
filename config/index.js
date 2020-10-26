4a25da36afbaf3c94abbc7c30650d9ea2c1b208b
shriya2603 <sundriyalshriya@gmail.com>
2020-10-26 20:59:25 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ module.exports = {
     JWT_SECRET: 'secret',
     oauth: {
       google: {
-        clientID: '250918021918-i29gm3bheergo1bh476ns2f194d5so8c.apps.googleusercontent.com',
-        clientSecret: 'sW1L2aXY345-hTbGTab_NjOL'
+        clientID: '',
+        clientSecret: ''
       }
     }
   };
